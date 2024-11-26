4832c91a85e15a90cd132ca40f1236c6d941f307
kaitcham <chamsf5@aol.com>
2024-11-26 09:42:36 +0200

    Initial commit from Create Next App

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.scss';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <p>Ironji Trucking Logistics Management System</p>
+    </div>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import './styles/globals.scss';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+});
+
+export const metadata: Metadata = {
+  title: 'Ironji',
+  description: 'Trucking Logistics Management System',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className}`}>{children}</body>
+    </html>
+  );
+}
