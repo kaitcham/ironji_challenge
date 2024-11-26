5c242f23f4035552e722d385cf1c6ea2c761acee
kaitcham <chamsf5@aol.com>
2024-11-26 16:47:00 +0200

    landing page information

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -23,6 +23,31 @@ export default function Home() {
           </div>
         </nav>
       </header>
+      <div className={styles.hero__info}>
+        <div className="info">
+          <h1>
+            <span>Trucking Logistics</span> <br /> Management System
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit nam fugiat, expedita fuga beatae magnam, labore
+            molestiae assumenda quod deleniti repellat excepturi! Atque rerum
+            eligendi iste expedita.
+          </p>
+          <Link href="/login" className={styles.hero__btn}>
+            Login/Register
+          </Link>
+        </div>
+        <div className={styles.hero__image}>
+          <Image
+            src="https://assets-global.website-files.com/660a742bdcbe8e8216996048/660a742bdcbe8e8216996152_car%20(2).png"
+            alt="Hero Image"
+            width={500}
+            height={500}
+            quality={100}
+          />
+        </div>
+      </div>
     </section>
   );
 }
