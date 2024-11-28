55e2f0e904db921679bcf3db3cf62f47883f34dd
kaitcham <chamsf5@aol.com>
2024-11-28 10:27:13 +0200

    authenticate users with Google OAuth

--- app/(root)/login/page.tsx
+++ app/(root)/login/page.tsx
@@ -1,10 +1,16 @@
 import '@/app/styles/_login.scss';
+import { signIn } from '@/auth';
 
 export default async function Login() {
   return (
     <div className="login__container">
       <h1>Login</h1>
-      <form>
+      <form
+        action={async () => {
+          'use server';
+          await signIn('google', { redirectTo: '/dashboard' });
+        }}
+      >
         <p>To continue, please sign in with your Google account.</p>
         <button type="submit">Sign in with Google</button>
       </form>
