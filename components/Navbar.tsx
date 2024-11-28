177e12861413b1c4633ce38028c029d460e511f5
kaitcham <chamsf5@aol.com>
2024-11-28 10:30:52 +0200

    remove login button on login page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,33 @@
+'use client';
 import Link from 'next/link';
 import Image from 'next/image';
 import ToggleTheme from './ToggleTheme';
+import { usePathname } from 'next/navigation';
 import '@/app/styles/_navbar.scss';
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <header className="header__container">
       <nav className="header__container__navbar">
         <Link href="/" className="logo">
-          <Image src="/logo.svg" alt="Company Logo" width={35} height={35} />
+          <Image
+            src="/logo.svg"
+            alt="Company Logo"
+            width={35}
+            height={35}
+            priority={true}
+          />
         </Link>
 
         <div className="right__section">
           <ToggleTheme sunIcon="sun__btn" moonIcon="moon__btn" />
-          <Link href="/login" className="login__btn">
-            Login
-          </Link>
+          {pathname !== '/login' && (
+            <Link href="/login" className="login__btn">
+              Login
+            </Link>
+          )}
         </div>
       </nav>
     </header>
