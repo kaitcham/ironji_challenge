59ca757a9b35a374fd5c1116a1a8b6dab3ff11db
kaitcham <chamsf5@aol.com>
2024-11-28 15:41:08 +0200

    protect resources with next middleware

--- auth.ts
+++ auth.ts
@@ -4,4 +4,5 @@ import Google from 'next-auth/providers/google';
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
   pages: { signIn: '/login' },
+  callbacks: { authorized: async ({ auth }) => !!auth },
 });
