81afec3ea039817ca4896df1beef0fc8263e89a1
kaitcham <chamsf5@aol.com>
2024-11-29 08:00:00 +0200

    install headlessui and config next.js to use sass implementation

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['assets-global.website-files.com'],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'assets-global.website-files.com' },
+    ],
+  },
+  sassOptions: {
+    implementation: 'sass',
+    includeCss: true,
   },
 };
 
