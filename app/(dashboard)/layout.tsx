795932d27e43640cbfb6a61cf3a6418511a489b2
kaitcham <chamsf5@aol.com>
2024-12-01 11:53:09 +0200

    react query provider

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,8 @@
 import '@/styles/_dashboard.scss';
 import TopNavbar from '@/components/TopNavbar';
 import SideNavbar from '@/components/SideNavbar';
+import ReactQueryProvider from '@/app/providers';
+import { TruckProvider } from '../context/TruckContext';
 
 export default function DashboardLayout({
   children,
@@ -12,7 +14,9 @@ export default function DashboardLayout({
         <div className="dashboard__inner__container__rightside">
           <div className="dashboard__inner__container__rightside__content">
             <TopNavbar />
-            {children}
+            <ReactQueryProvider>
+              <TruckProvider>{children}</TruckProvider>
+            </ReactQueryProvider>
           </div>
         </div>
       </div>
