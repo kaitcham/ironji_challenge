da231bd4f9dfcba7d0f024f201966ba95a288ad0
kaitcham <chamsf5@aol.com>
2024-12-02 08:56:13 +0200

    server action to create a new truck

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { Truck } from './types';
+import { Truck, TruckStatus } from './types';
 
 const API_URL = process.env.API_URL;
 
@@ -8,5 +8,16 @@ type FilterType = { query?: string };
 
 export async function getTrucks({ query }: FilterType): Promise<Truck[]> {
   const response = await fetch(`${API_URL}/trucks?${query}`);
+  return await response.json();
+}
+
+export async function createTruck(
+  truck: Omit<Truck, 'status'>
+): Promise<Truck> {
+  const newTruck = { ...truck, status: TruckStatus.AVAILABLE };
+  const response = await fetch(`${API_URL}/trucks`, {
+    method: 'POST',
+    body: JSON.stringify(newTruck),
+  });
   return await response.json();
 }
