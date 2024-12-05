5765c2918d9efdc8931ab856d7e262154f7b2aeb
kaitcham <chamsf5@aol.com>
2024-12-05 09:50:00 +0200

    add component to update truck state

---
+++ components/TruckStateToggle.tsx
@@ -0,0 +1,77 @@
+import { toast } from 'sonner';
+import { Truck, TruckStatus } from '@/lib/types';
+import { updateTruckStatus } from '@/lib/actions';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import '@/styles/_trucks.scss';
+
+export default function TruckStateToggle({
+  truck,
+  selectedOption,
+}: {
+  truck: Truck;
+  selectedOption: string;
+}) {
+  const queryClient = useQueryClient();
+  const updateStatusMutation = useMutation({
+    mutationFn: ({ id, status }: { id: string; status: TruckStatus }) =>
+      updateTruckStatus(id, status),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['trucks', selectedOption] });
+      return toast.success('Status updated successfully');
+    },
+    onError: (error: Error) => {
+      return toast.error(error.message);
+    },
+  });
+
+  const handleChange = (id: string, newStatus: TruckStatus) => {
+    updateStatusMutation.mutate({ id, status: newStatus });
+  };
+
+  const statusOptions = {
+    [TruckStatus.AVAILABLE]: [
+      {
+        label: 'Delivering',
+        action: () => handleChange(truck.id, TruckStatus.DELIVERING),
+      },
+      {
+        label: 'Maintenance',
+        action: () => handleChange(truck.id, TruckStatus.MAINTENANCE),
+      },
+    ],
+    [TruckStatus.DELIVERING]: [
+      {
+        label: 'Delivering',
+        action: () => handleChange(truck.id, TruckStatus.AVAILABLE),
+      },
+    ],
+    [TruckStatus.MAINTENANCE]: [
+      {
+        label: 'Maintenance',
+        action: () => handleChange(truck.id, TruckStatus.AVAILABLE),
+      },
+    ],
+  };
+
+  return (
+    <div className="flex justify-between items-center">
+      {statusOptions[truck.status]?.map(({ label, action }) => (
+        <div key={label} className="truck__card__footer">
+          <input
+            name="status"
+            type="checkbox"
+            onChange={action}
+            disabled={updateStatusMutation.isPending}
+            checked={truck.status !== TruckStatus.AVAILABLE}
+            className={
+              truck.status === TruckStatus.MAINTENANCE
+                ? 'accent-red-500/50'
+                : ''
+            }
+          />
+          <label htmlFor="status">{label}</label>
+        </div>
+      ))}
+    </div>
+  );
+}
