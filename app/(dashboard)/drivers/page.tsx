a6b09b85534bb810f07ccec7ab163f4aea43c46a
kaitcham <chamsf5@aol.com>
2024-12-06 10:40:57 +0200

    update drivers page to display a table of all drivers

--- app/(dashboard)/drivers/page.tsx
+++ app/(dashboard)/drivers/page.tsx
@@ -1,9 +1,86 @@
-import AllTrucksFilters from '@/components/Filters';
+'use client';
+import Filters from '@/components/Filters';
+import Loading from '@/components/Loading';
+import { useDrivers } from '@/context/DriverContext';
+import Error from '@/components/Error';
+import '@/styles/_trucks.scss';
 
 export default function page() {
+  const options = ['All Drivers'];
+  const { isPending, error, drivers, selectedOption, SetSelectedOption } =
+    useDrivers();
+
+  if (isPending) return <Loading />;
+  if (error) return <Error error={error} />;
+
   return (
     <div className="rightside__content__body__header">
       <h1>Drivers</h1>
+      <Filters
+        name="Driver"
+        options={options}
+        initialData={drivers!}
+        selectedOption={selectedOption}
+        SetItemToEdit={() => {}}
+        SetSelectedOption={SetSelectedOption}
+      />
+      <div className="overflow-auto mt-6 lg:max-w-4xl rounded-t-lg">
+        <table className="w-full h-auto divide-y-2 divide-gray-200 bg-white text-sm">
+          <thead className="[&>tr]:text-left">
+            <tr>
+              <th className="px-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                Name
+              </th>
+              <th className="px-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                License Number
+              </th>
+              <th className="px-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                Contact Number
+              </th>
+              <th className="px-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                Assigned Truck
+              </th>
+              <th className="px-4 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                Actions
+              </th>
+            </tr>
+          </thead>
+
+          <tbody className="divide-y divide-gray-200">
+            {drivers?.map((driver) => (
+              <tr>
+                <td className="whitespace-nowrap px-4 py-3 text-gray-900">
+                  {driver.name}
+                </td>
+                <td className="whitespace-nowrap px-4 py-3 text-gray-700">
+                  {driver.license_number}
+                </td>
+                <td className="whitespace-nowrap px-4 py-3 text-gray-700">
+                  {driver.contact_number}
+                </td>
+                <td className="whitespace-nowrap px-4 py-3 text-gray-700">
+                  {driver.assigned_truck?.plate_number ?? (
+                    <button
+                      popoverTarget="assign-truck"
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      + Truck
+                    </button>
+                  )}
+                </td>
+                <td className="flex items-center whitespace-nowrap px-4 py-3 text-gray-700">
+                  <button className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                    Edit
+                  </button>
+                  <button className="font-medium text-red-600 dark:text-red-500 hover:underline ms-4">
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
